089e09ba5a75653cdf69920ee7d142764037aab7
jratke <jratke@cps.edu>
2025-03-25 16:04:11 -0500

    Install AdvantageKit and dependencies.

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class Constants {
+    public static final Mode simMode = Mode.SIM;
+    public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+
+    public static enum Mode {
+        REAL, // Running on a real robot.
+        SIM, // Running a physics simulator.
+        REPLAY // Replaying from a log file.
+    }
+}
