576dfa7c9c34600bb4310b2e2f84c33ceb0ac593
Camden Rush <64759244+camden-git@users.noreply.github.com>
2025-08-28 15:30:20 -0500

    jabobi

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,40 +1,54 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean
- * constants. This class should not be used for any other purpose. All constants
- * should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the
- * constants are needed, to reduce verbosity.
- */
-public final class Constants {
-  public static final class DriveConstants {
-    public static final int LEFT_LEADER_ID = 4;
-    public static final int LEFT_FOLLOWER_ID = 3;
-    public static final int RIGHT_LEADER_ID = 5;
-    public static final int RIGHT_FOLLOWER_ID = 2;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
 
-    public static final int DRIVE_MOTOR_CURRENT_LIMIT = 60;
-  }
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.RobotBase;
 
-  public static final class RollerConstants {
-    public static final int ROLLER_MOTOR_ID = 1;
-    public static final int ROLLER_MOTOR_CURRENT_LIMIT = 60;
-    public static final double ROLLER_MOTOR_VOLTAGE_COMP = 10;
-    public static final double ROLLER_EJECT_VALUE = 0.44;
-  }
+public class Constants {
+    public static final Mode simMode = Mode.SIM;
+    public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
 
-  public static final class OperatorConstants {
-    public static final int DRIVER_CONTROLLER_PORT = 0;
-    public static final int OPERATOR_CONTROLLER_PORT = 1;
-  }
+    public static final int driveControllerId = 0;
+    public static final int pdhCANId = 1;
+
+    public static class ArmConstants {
+        public static final int pivotMotorID = 8;
+        public static final int WheelMotorID = 9;
+    }
+
+    public static class RollerConstants {
+        public static final int rollerMotorID = 1;
+        public static final double wheelDutyCycle = 0.5;
+    }
+
+    public static class DrivetrainConstants {
+        // Smart current limit (amps) per motor
+        public static final int currentLimit = 60;
+
+        public static final double p = 100;
+        public static final double i = 0;
+        public static final double d = 5;        
+
+        public static final int leftMotorAID = 2;
+        public static final int leftMotorBID = 5;
+        public static final int rightMotorAID = 3;
+        public static final int rightMotorBID = 4;
+
+        public static final int pigeonID = 7;
+
+        public static final Distance trackWidth = Inches.of(21.5);
+        public static final Distance wheelRadius = Inches.of(3.0);
+        public static final double motorReduction = 10.71;
+
+        public static final double positionConversion = Math.PI * 2 * wheelRadius.in(Meters);
+        public static final double velocityConversion = Math.PI * 2 * wheelRadius.in(Meters) / 60;
+    }
+
+    public static enum Mode {
+        REAL, // Running on a real robot.
+        SIM, // Running a physics simulator.
+        REPLAY // Replaying from a log file.
+    }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,177 +3,123 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-import frc.robot.Constants;
-import frc.robot.Constants.DriveConstants;
 
-import java.io.Console;
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+import org.littletonrobotics.urcl.URCL;
 
-import com.ctre.phoenix6.hardware.Pigeon2;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+public class Robot extends LoggedRobot {
+  private Command m_autonomousCommand;
 
-import frc.robot.subsystems.CANRollerSubsystem;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.LimelightHelpers;
+  private final RobotContainer m_robotContainer;
 
-
-/**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
- * this project, you must also update the manifest file in the resource directory.
- */
-public class Robot extends TimedRobot {
-  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-  NetworkTableEntry tx = table.getEntry("tx");
-  NetworkTableEntry ty = table.getEntry("ty");
-  NetworkTableEntry ta = table.getEntry("ta");
-
-
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  private double autoturnspeed = 0.5;
-  private double autospeed = 0.5;
-  private LimelightHelpers limelight;
-  private double slowdownamount = 0;
-  private boolean bDown = false;
- // The motors on the left side of the drive.
- private final SparkMax m_leftLeader = new SparkMax(DriveConstants.LEFT_LEADER_ID, MotorType.kBrushed);
- private final SparkMax m_leftFollower = new SparkMax(DriveConstants.LEFT_FOLLOWER_ID, MotorType.kBrushed);
-
- // The motors on the right side of the drive.
- private final SparkMax m_rightLeader = new SparkMax(DriveConstants.RIGHT_LEADER_ID, MotorType.kBrushed);
- private final SparkMax m_rightFollower = new SparkMax(DriveConstants.RIGHT_FOLLOWER_ID, MotorType.kBrushed);
-
- 
-  private final XboxController m_controller = new XboxController(0);
-  private final Timer m_timer = new Timer();
-  // The robot's drive
-  private final DifferentialDrive m_drive;
-  private final CANRollerSubsystem rollerSubsystem = new CANRollerSubsystem();
-
-
-  // The gyro sensor
-  private final Pigeon2 m_gyro = new Pigeon2(8);
-  private final SparkMaxConfig m_dConfig = new SparkMaxConfig();
-
-
-
-
-  // Odometry class for tracking robot pose
-
-  /** Called once at the beginning of the robot program. */
   public Robot() {
-    m_dConfig.follow(m_leftLeader);
-    m_leftFollower.configure(m_dConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_dConfig.follow(m_rightLeader);
-    m_rightFollower.configure(m_dConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // Record metadata
+    Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+    Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+    Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+    Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+    Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+    switch (BuildConstants.DIRTY) {
+      case 0:
+        Logger.recordMetadata("GitDirty", "All changes committed");
+        break;
+      case 1:
+        Logger.recordMetadata("GitDirty", "Uncomitted changes");
+        break;
+      default:
+        Logger.recordMetadata("GitDirty", "Unknown");
+        break;
+    }
 
-    m_dConfig.disableFollowerMode();
-    m_rightLeader.configure(m_dConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_leftLeader.configure(m_dConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // Set up data receivers & replay source
+    switch (Constants.currentMode) {
+      case REAL:
+        // Running on a real robot, log to a USB stick ("/U/logs")
+        Logger.addDataReceiver(new WPILOGWriter());
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
 
-    m_drive = new DifferentialDrive(m_leftLeader, m_rightLeader);
+      case SIM:
+        // Running a physics simulator, log to NT
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+
+      case REPLAY:
+        // Replaying a log, set up replay source
+        setUseTiming(false); // Run as fast as possible
+        String logPath = LogFileUtil.findReplayLog();
+        Logger.setReplaySource(new WPILOGReader(logPath));
+        Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+        break;
+    }
+
+    // Initialize URCL
+    Logger.registerURCL(URCL.startExternal());
+
+    // Start AdvantageKit logger
+    Logger.start();
+
+    m_robotContainer = new RobotContainer();
   }
 
   @Override
-  public void robotPeriodic(){
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
   }
-  /** This function is run once each time the robot enters autonomous mode. */
-  @Override 
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void disabledExit() {}
+
+  @Override
   public void autonomousInit() {
-    
-    LimelightHelpers.setLEDMode_PipelineControl("limelight");
-    LimelightHelpers.setLEDMode_PipelineControl("limelight");
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-    LimelightHelpers.setLEDMode_PipelineControl("limelight");
-
-    LimelightHelpers.setLEDMode_PipelineControl("limelight");
-
-    LimelightHelpers.setLEDMode_ForceBlink("limelight");
-    m_timer.restart();
-    
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {
+  public void autonomousPeriodic() {}
 
-
-    double x = tx.getDouble(0.0);
-    double y = ty.getDouble(0.0);
-    double area = ta.getDouble(0.0);
-
-    //post to smart dashboard periodically
-    SmartDashboard.putNumber("LimelightX", x);
-    SmartDashboard.putNumber("LimelightY", y);
-    SmartDashboard.putNumber("LimelightArea", area);
-      
-      if (Math.abs(x) > 5) {
-        if (Math.signum(x) == -1){
-          //m_drive.tankDrive(autoturnspeed, autoturnspeed);
-        }
-        if (Math.signum(x) == 1){
-          //m_drive.tankDrive(-autoturnspeed, -autoturnspeed);
-        }
-        
-      } else {
-        //m_drive.tankDrive(autospeed, -autospeed);
-      }
-  }
-  /** This function is called once each time the robot enters teleoperated mode. */
   @Override
-  public void teleopInit() {}
+  public void autonomousExit() {}
 
-  /** This function is called periodically during teleoperated mode. */
   @Override
-  public void teleopPeriodic() {
-    double x = Math.signum(m_controller.getRightX())*((m_controller.getRightX()*m_controller.getRightX()));
-    double y = m_controller.getLeftY()*-1;
-    
-
-    if (Math.abs(x) <= 0.4){x = 0;}
-    if (Math.abs(y) <= 0.4){y = 0;}
-    slowdownamount = 1-(0.8 * m_controller.getLeftTriggerAxis());
-    System.out.println(slowdownamount);
-    /*(-x/2) + (y/2),  (-x/2) - (y/2) */
-    if (m_controller.getYButton()) {
-      rollerSubsystem.runRoller(1.0, 0.0);
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
     }
-    if (m_controller.getBButtonPressed()){
-      bDown = true;
-    }
-    
-    else if(m_controller.getAButtonReleased()){
-      bDown = false;
-    }
-    if (bDown){
-      m_drive.tankDrive(((-x/2) + (y/2))*2*slowdownamount,  ((-x/2) - (y/2))*2*slowdownamount);
-
-    }
-    else{
-      m_drive.tankDrive(((-x/2) + (y/2))*slowdownamount, ((-x/2) - (y/2))*slowdownamount);
-    }
-    m_controller.setRumble(RumbleType.kRightRumble, Math.abs(y));
-    m_controller.setRumble(RumbleType.kLeftRumble, Math.abs(x));
   }
 
-  /** This function is called once each time the robot enters test mode. */
   @Override
-  public void testInit() {}
+  public void teleopPeriodic() {}
 
-  /** This function is called periodically during test mode. */
+  @Override
+  public void teleopExit() {}
+
+  @Override
+  public void testInit() {
+    CommandScheduler.getInstance().cancelAll();
+  }
+
   @Override
   public void testPeriodic() {}
+
+  @Override
+  public void testExit() {}
 }
